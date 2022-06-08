8354e4b02e7d053ad5730fa4583944f83210ad0d
oliverweissl <o.weissl@student.vu.nl>
2022-06-08 16:47:24 +0200

    v0.01
    - flocking not working
    - values calculated right??

---
+++ Flocking/flocking.py
@@ -0,0 +1,106 @@
+from enum import Enum, auto
+
+import numpy as np
+import pygame as pg
+from pygame.math import Vector2
+from vi import Agent, Simulation
+from vi.config import Config, dataclass, deserialize
+
+
+@deserialize
+@dataclass
+class FlockingConfig(Config):
+    alignment_weight: float = 0.5
+    cohesion_weight: float = 0.5
+    separation_weight: float = 0.5
+
+    delta_time: float = 3
+
+    mass: int = 20
+
+    def weights(self) -> tuple[float, float, float]:
+        return (self.alignment_weight, self.cohesion_weight, self.separation_weight)
+
+
+class Bird(Agent):
+    config: FlockingConfig
+
+    def change_position(self):
+        # Pac-man-style teleport to the other end of the screen when trying to escape
+        self.there_is_no_escape()
+        #YOUR CODE HERE -----------
+        n = list(self.in_proximity_accuracy())
+        if amt := len(n) > 0:
+            speeds = [s[0].move[1] for s in n]
+            avg_speed = (1/amt)*sum(speeds)
+            pos = [s[0].pos for s in n]
+
+            avg_pos = np.average(pos,axis = 0)
+            fc = avg_pos-self.pos
+
+
+            coheison = fc - self.move[1]
+            seperation = np.average([self.pos - x for x in pos],axis = 0)
+            alignment = avg_speed-self.move[1]
+
+            f_total = (alignment+seperation+coheison)
+            self.move += f_total
+
+        self.pos = self.pos + self.move
+
+
+
+
+        #END CODE -----------------
+
+
+class Selection(Enum):
+    ALIGNMENT = auto()
+    COHESION = auto()
+    SEPARATION = auto()
+
+
+class FlockingLive(Simulation):
+    selection: Selection = Selection.ALIGNMENT
+    config: FlockingConfig
+
+    def handle_event(self, by: float):
+        if self.selection == Selection.ALIGNMENT:
+            self.config.alignment_weight += by
+        elif self.selection == Selection.COHESION:
+            self.config.cohesion_weight += by
+        elif self.selection == Selection.SEPARATION:
+            self.config.separation_weight += by
+
+    def before_update(self):
+        super().before_update()
+
+        for event in pg.event.get():
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_UP:
+                    self.handle_event(by=0.1)
+                elif event.key == pg.K_DOWN:
+                    self.handle_event(by=-0.1)
+                elif event.key == pg.K_1:
+                    self.selection = Selection.ALIGNMENT
+                elif event.key == pg.K_2:
+                    self.selection = Selection.COHESION
+                elif event.key == pg.K_3:
+                    self.selection = Selection.SEPARATION
+
+        a, c, s = self.config.weights()
+        print(f"A: {a:.1f} - C: {c:.1f} - S: {s:.1f}")
+
+
+(
+    FlockingLive(
+        FlockingConfig(
+            image_rotation=True,
+            movement_speed=1,
+            radius=50,
+            seed=1,
+        )
+    )
+    .batch_spawn_agents(50, Bird, images=["images/bird.png"])
+    .run()
+)
