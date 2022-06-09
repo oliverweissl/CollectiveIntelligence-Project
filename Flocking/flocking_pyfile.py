b8974d253a17d256209b92ee647ad5cfb68049cd
Oliver <o.weissl@student.vu.nl>
2022-06-09 21:16:39 +0200

    small runtime improvements

--- Flocking/flocking_pyfile.py
+++ Flocking/flocking_pyfile.py
@@ -4,12 +4,7 @@ from matplotlib import pyplot as plt
 import pandas as pd
 
 df = pd.read_csv("Experiments/A0.75_C0.40_S0.40_W0.50.csv")
-print(df)
-
-in_flocks = []
-angles = []
-clusters_num_tt = []
-indiv_tt = []
+df["cluster"] = None
 
 def find_in_cluster(cl, idx):
     for k, cluster in cl.items():
@@ -17,43 +12,40 @@ def find_in_cluster(cl, idx):
             return k
     return None
 
-def neighbours(indiv, i, curr_cluster, counter):
+def neighbours(indiv, i, curr_cluster, frame):
     ind = indiv[i]
     reached_ind = []
     for i_2 in range(len(indiv)):
         if indiv[i_2][5]:
             continue
-
-        if i_2 != i and \
-                np.deg2rad(np.abs(ind[2] - indiv[i_2][2])) < 0.5 and \
-                np.linalg.norm(ind[:2] - indiv[i_2][:2]) <= 25:
+        if i_2 != i and np.deg2rad(np.abs(ind[2] - indiv[i_2][2])) < 0.5 and np.linalg.norm(ind[:2] - indiv[i_2][:2]) <= 25:
             indiv[i_2][5] = curr_cluster
-            if counter == 128:
-                print(i, ind, i_2, indiv[i_2])
             reached_ind.append(i_2)
+            """
+            if frame == 128:
+                print(i, ind, i_2, indiv[i_2])
+            """
+
     for j in reached_ind:
-        neighbours(indiv, j, curr_cluster, counter)    
+        neighbours(indiv, j, curr_cluster, frame)
     curr_cluster += 1
     return indiv, curr_cluster
 
-counter = 0
+
+indiv_tt = []
+clusters_num_tt = []
 
 for frame in tqdm(range(max(df["frame"]))):
     curr_cluster = 1
-    in_flocks.append(df["image_index"][(df["frame"]==frame) & (df["image_index"]==1)].count())
-    #print(in_flocks)
-    indiv = df[["x","y","angle", "image_index", "id"]][df["frame"]==frame]#.to_numpy()#.reshape(-1, 1)
-    indiv["cluster"] = None
-    indiv = indiv.to_numpy()
-    
-    for i in range(len(indiv)):
-        if indiv[i][5]:
+    indiv = df[["x","y","angle", "image_index", "id","cluster"]][df["frame"]==frame].to_numpy()
+    for i,ind in enumerate(indiv):
+        if ind[5]:
             continue
-        indiv[i][5] = curr_cluster
-        indiv, curr_cluster = neighbours(indiv, i, curr_cluster, counter)
-        #print(indiv[:, 4])
+        ind[5] = curr_cluster
+        indiv, curr_cluster = neighbours(indiv, i, ind[5], frame)
+
+
     clusters_num_tt.append(curr_cluster)
-    counter += 1
     indiv_tt.append(indiv)
 print(clusters_num_tt)
 
