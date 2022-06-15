4add16cabdc74d9f41f6bfc09b5cc0667b9b0be6
Stefan Vasilev <ste5vas@gmail.com>
2022-06-15 10:47:01 +0200

    first

--- Aggregation/aggregation.py
+++ Aggregation/aggregation.py
@@ -4,16 +4,20 @@ import numpy as np
 import pygame as pg
 import pygame.camera as pgc
 from pygame.math import Vector2
-from vi import Agent, Simulation
+from pyparsing import Optional
+from vi import Agent, Simulation, HeadlessSimulation
 from vi.config import Config, dataclass, deserialize
+import math
 
 @deserialize
 @dataclass
 class AggregationConfig(Config):
     random_weight = 3
-
+    circle_area = math.pi*Config.radius**2
     delta_time: float = 2
     mass: int = 20
+    timer_j = 100./delta_time
+    D = 20
 
     def weights(self) -> tuple[float, float, float, float]:
         return (self.alignment_weight, self.cohesion_weight, self.separation_weight, self.random_weight)
@@ -21,10 +25,32 @@ class AggregationConfig(Config):
 
 class Cockroach(Agent):
     config: AggregationConfig
+        
+    def __init__(self,  *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.mean_neighbours = 0
+        self.ROLLING_SIZE = 1 
+        self.timer_j_counting = self.config.timer_j
+        self.timer_j_started = False
+        self.timer_j_picked = None
+        self.leaving = False
+        self.p_leave = None
+        self.p_join = None
+
+    def _collect_replay_data(self):
+        super()._collect_replay_data()
+        self._Agent__simulation._metrics._temporary_snapshots["p_leave"].append(self.p_leave)
+        self._Agent__simulation._metrics._temporary_snapshots["p_join"].append(self.p_join)
+        
     def change_position(self):
         n = list(self.in_proximity_accuracy()) #list of neighbors
-        leave = 0.3 **  len(n) if len(n) > 0 else 0.3
-
+        num_n = len(n)
+        self.mean_neighbours = (self.mean_neighbours * (self.ROLLING_SIZE-1) + num_n) / self.ROLLING_SIZE
+        leave = (1-self.mean_neighbours/self.config.circle_area*100) ** (num_n*1.3**min(self.mean_neighbours, 0.0)) if num_n > 0 else 0.3
+        #print(leave, self.mean_neighbours/self.config.circle_area, num_n)
+        #print(leave)
+        self.p_leave = leave
+        
         """
         if len(n) > 1:
             if np.random.uniform() > 0.999:
@@ -42,15 +68,35 @@ class Cockroach(Agent):
                 self.move = nm / np.linalg.norm(nm) #normalize vector
 
         if self.on_site():
-            if np.random.uniform() > 0.9:
-                self.move = pg.Vector2((0,0))
-            if np.linalg.norm(self.move) < 0.1 and np.random.uniform() > (1-leave):
+            if not self.timer_j_started and not self.leaving:
+                self.timer_j_started = True
+                self.timer_j_picked = np.random.uniform(0, self.config.timer_j)
+            elif not self.leaving: 
+                if int(self.timer_j_counting) <= self.timer_j_picked:
+                    self.timer_j_started = False
+                    self.timer_j_picked = None
+                    self.timer_j_counting = self.config.timer_j
+                    self.move = pg.Vector2((0,0))
+                
+                self.timer_j_counting -= 1
+            
+            if np.random.uniform() < 1/self.config.D and np.random.uniform() <= leave and not self.leaving and not self.timer_j_started:
+                self.leaving = True
                 self.move = np.random.uniform(low = -1, high = 1, size = 2)
+                self.move = self.move / np.linalg.norm(self.move) if np.linalg.norm(self.move) > 0 else self.move
+            #if np.random.uniform() < 0.1:
+            #    self.move = pg.Vector2((0,0))
+            #if np.linalg.norm(self.move) < 0.1 and np.random.uniform() > (1-leave):
+            #    self.move = np.random.uniform(low = -1, high = 1, size = 2)
+            
         else:
+            if self.leaving:
+                self.leaving = False
             self.move = self.move / np.linalg.norm(self.move) if np.linalg.norm(self.move) > 0 else self.move
             f_total = (self.config.random_weight * np.random.uniform(low = -1, high = 1, size = 2))/self.config.mass
             self.move += f_total
-
+        
+        self.ROLLING_SIZE += 1
         self.pos += self.move * self.config.delta_time  #update pos
 
 
@@ -68,11 +114,12 @@ df = (
         )
     )
     .spawn_obstacle("images/boundary.png", x//2,  y//2)
-    .spawn_site("images/shadow_norm.png", x//2 , y//2)
+    .spawn_site("images/shadow_norm.png", 150 , y//2)
+    .spawn_site("images/shadow_norm.png", x-150 , y//2)
     .batch_spawn_agents(50, Cockroach, images=["images/white.png","images/red.png"])
     .run()
 )
 
 
 dfs = df.snapshots
-#dfs.write_csv(f"Experiments/A{FlockingConfig.alignment_weight:.2f}_C{FlockingConfig.cohesion_weight:.2f}_S{FlockingConfig.separation_weight:.2f}_W{FlockingConfig.random_weight:.2f}.csv")
+dfs.write_csv(f"Experiments/A.csv")
