522c09e2e135f693909a81c42f52744afc0acc41
stefanvasilev <31345149+stefanvasilev@users.noreply.github.com>
2022-06-16 14:37:20 +0200

    densitymod

--- Aggregation/aggregation.py
+++ Aggregation/aggregation.py
@@ -17,7 +17,17 @@ class AggregationConfig(Config):
     delta_time: float = 2
     mass: int = 20
     timer_j = 100./delta_time
-    D = 20
+    D = 1
+
+    weigth_leave = 0.4 #gamma
+    weight_join = 0.5
+
+    #params for search
+    join_param = 0.9
+    loneliness_v1 = -0.4
+    loneliness_v2 = 3
+
+    mermory = 10 
 
     def weights(self) -> tuple[float, float, float, float]:
         return (self.alignment_weight, self.cohesion_weight, self.separation_weight, self.random_weight)
@@ -36,6 +46,11 @@ class Cockroach(Agent):
         self.leaving = False
         self.p_leave = None
         self.p_join = None
+        self.loneliness = 0.5
+        self.on_place = 0
+        self.last_move = pg.Vector2((0,0))
+        self.last_seen = [0]*self.config.mermory
+        self.age = 1
 
     def _collect_replay_data(self):
         super()._collect_replay_data()
@@ -44,21 +59,43 @@ class Cockroach(Agent):
         
     def change_position(self):
         n = list(self.in_proximity_accuracy()) #list of neighbors
-        num_n = len(n)
-        self.mean_neighbours = (self.mean_neighbours * (self.ROLLING_SIZE-1) + num_n) / self.ROLLING_SIZE
-        leave = (1-self.mean_neighbours/self.config.circle_area*100) ** (num_n*1.3**min(self.mean_neighbours, 0.0)) if num_n > 0 else 0.3
-        #print(leave, self.mean_neighbours/self.config.circle_area, num_n)
-        #print(leave)
-        self.p_leave = leave
+        len_n = len(n)
+        self.last_seen.append(len_n)
+        self.last_seen.pop(0)
+        self.mean_neighbours = (self.mean_neighbours * (self.ROLLING_SIZE-1) + len_n) / self.ROLLING_SIZE
+        density = self.mean_neighbours/self.config.circle_area
         
-        """
-        if len(n) > 1:
-            if np.random.uniform() > 0.999:
-                self.move = pg.Vector2((0,0))
-            else: self.move *= (1 - len(n))
-            if np.linalg.norm(self.move) < 0.1 and np.random.uniform() > (1-leave):
-                self.move = np.random.uniform(low = -1, high = 1, size = 2)
-        """
+        self.loneliness = 1/(1+np.exp(self.config.loneliness_v1*np.average(self.last_seen)+self.config.loneliness_v2))*math.log(self.age)
+
+        P_join = 0
+        P_leave = (1+np.log(1+density)*300) * self.config.weigth_leave / (len_n ** math.log(len_n)) if len_n > 0 else self.config.weigth_leave
+        #print(P_leave, (1+np.log(1+density)*100))
+
+        #if self.on_site():
+        #    self.on_place  +=2
+        #    P_join = self.config.weight_join
+        if len(n) > 2:
+            self.on_place+=1
+            P_join = 1-((1+np.log(1+density)*40)*self.config.join_param**math.log(len_n))
+        else: self.on_place=0
+
+
+        self.move = self.move / np.linalg.norm(self.move) if np.linalg.norm(self.move) > 0 else self.move
+        f_total = (self.config.random_weight * np.random.uniform(low = -1, high = 1, size = 2))/self.config.mass
+        self.move += f_total
+
+
+        if np.random.uniform() < P_join:
+            self.move *= self.config.weigth_leave ** math.log(self.on_place) #experiment with slowing
+
+        if np.linalg.norm(self.move) < 0.4:
+            self.move = pg.Vector2((0,0))
+            self.change_image(1)
+            #self.change_image(self.on_site_id()+2) if self.on_site_id() is not None else self.change_image(1)
+            self.move = np.random.uniform(low = -0.2, high = 0.2, size = 2)*(1-self.loneliness) if np.random.uniform() < P_leave and np.random.uniform() < 1/self.config.D else self.move
+        else: self.change_image(0)
+
+
 
         #collision detection
         coll = list(self.obstacle_intersections(scale = 2))
@@ -67,37 +104,8 @@ class Cockroach(Agent):
                 nm = self.move-(c-self.pos) #current move velocity - distance to the obstacle
                 self.move = nm / np.linalg.norm(nm) #normalize vector
 
-        if self.on_site():
-            if not self.timer_j_started and not self.leaving:
-                self.timer_j_started = True
-                self.timer_j_picked = np.random.uniform(0, self.config.timer_j)
-            elif not self.leaving: 
-                if int(self.timer_j_counting) <= self.timer_j_picked:
-                    self.timer_j_started = False
-                    self.timer_j_picked = None
-                    self.timer_j_counting = self.config.timer_j
-                    self.move = pg.Vector2((0,0))
-                
-                self.timer_j_counting -= 1
-            
-            if np.random.uniform() < 1/self.config.D and np.random.uniform() <= leave and not self.leaving and not self.timer_j_started:
-                self.leaving = True
-                self.move = np.random.uniform(low = -1, high = 1, size = 2)
-                self.move = self.move / np.linalg.norm(self.move) if np.linalg.norm(self.move) > 0 else self.move
-            #if np.random.uniform() < 0.1:
-            #    self.move = pg.Vector2((0,0))
-            #if np.linalg.norm(self.move) < 0.1 and np.random.uniform() > (1-leave):
-            #    self.move = np.random.uniform(low = -1, high = 1, size = 2)
-            
-        else:
-            if self.leaving:
-                self.leaving = False
-            self.move = self.move / np.linalg.norm(self.move) if np.linalg.norm(self.move) > 0 else self.move
-            f_total = (self.config.random_weight * np.random.uniform(low = -1, high = 1, size = 2))/self.config.mass
-            self.move += f_total
-        
-        self.ROLLING_SIZE += 1
         self.pos += self.move * self.config.delta_time  #update pos
+        self.age+=1
 
 
 class AggregationLive(Simulation):
@@ -111,12 +119,13 @@ df = (
             movement_speed=1,
             radius=50,
             seed=1,
+            fps_limit=0
         )
     )
     .spawn_obstacle("images/boundary.png", x//2,  y//2)
-    .spawn_site("images/shadow_norm.png", 150 , y//2)
-    .spawn_site("images/shadow_norm.png", x-150 , y//2)
-    .batch_spawn_agents(50, Cockroach, images=["images/white.png","images/red.png"])
+    #.spawn_site("images/shadow_norm.png", 150 , y//2)
+    #.spawn_site("images/shadow_norm.png", x-150 , y//2)
+    .batch_spawn_agents(200, Cockroach, images=["images/white.png","images/red.png"])
     .run()
 )
 
