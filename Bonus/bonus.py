7eca7991d14ac71a039dd23c9c6da234ef99c381
Oliver <o.weissl@student.vu.nl>
2022-06-21 10:17:20 +0200

    finnished_case

--- Bonus/bonus.py
+++ Bonus/bonus.py
@@ -11,9 +11,17 @@ GLOBAL_SEED = 1
 @deserialize
 @dataclass
 class Conf(Config):
+<<<<<<< Updated upstream
 
 
     random_weight = 3
+=======
+    alignment_weight: float = 0.50
+    cohesion_weight: float = 0.2
+    separation_weight: float = 0.25
+
+    random_weight = 1.3
+>>>>>>> Stashed changes
     delta_time: float = 2
     mass: int = 20
 
@@ -41,11 +49,29 @@ class Fox(Agent):
 
     def random_move(self):
         self.move = self.move / np.linalg.norm(self.move) if np.linalg.norm(self.move) > 0 else self.move
+<<<<<<< Updated upstream
         f_total = (self.config.random_weight * np.random.uniform(low = -1, high = 1, size = 2))/self.config.mass
+=======
+
+        if len(self.f) > 0:
+            pos = [s[0].pos for s in self.f]
+            vec = [s[0].move for s in self.f]
+
+            c = (np.average(pos,axis = 0) - self.pos) - self.move #fc - vel --> coheison
+            s = np.average([self.pos - x for x in pos], axis = 0) #seperation
+            a = np.average(vec, axis = 0) - self.move #alignment
+
+            f_total = (self.config.alignment_weight * a +
+                       self.config.separation_weight * s +
+                       self.config.cohesion_weight * c +
+                       self.config.random_weight * np.random.uniform(low = -1, high = 1, size = 2)) / self.fox_mass
+
+        else: f_total = (self.config.random_weight * np.random.uniform(low = -1, high = 1, size = 2)) / self.fox_mass
+>>>>>>> Stashed changes
         self.move += f_total
 
-    def update_location(self):
         self.pos += self.move * self.config.delta_time
+
 
     def change_position(self):
         self.change_image(1)
@@ -53,6 +79,7 @@ class Fox(Agent):
         if self.age > self.max_age or self.hunger <= 0: self.kill()
 
         if self.is_alive():
+<<<<<<< Updated upstream
             r = list(set(self.in_proximity_accuracy().filter_kind(Rabbit)))
             if len(r) > 0:
                 r[0][0].kill()
@@ -65,6 +92,24 @@ class Fox(Agent):
 
             self.age+=1
             self.hunger-=1
+=======
+            self.p_reproduce = 1/self.energy
+            self.energy *= 0.94
+
+
+
+            n = self.in_proximity_accuracy()
+            r = list(n.filter_kind(Rabbit))
+            self.f = list(n.filter_kind(Fox))
+            if len(r) > 0:
+                r[0][0].kill()
+                self.energy = min(300, self.energy+40)
+                if np.random.uniform() < self.p_reproduce:
+                    self.reproduce()
+
+            self.change_image(1)
+            self.random_move()
+>>>>>>> Stashed changes
 
 
 
@@ -85,21 +130,44 @@ class Rabbit(Agent):
 
     def random_move(self):
         self.move = self.move / np.linalg.norm(self.move) if np.linalg.norm(self.move) > 0 else self.move
-        f_total = (self.config.random_weight * np.random.uniform(low = -1, high = 1, size = 2))/self.config.mass
+
+        if len(self.r) > 0:
+            pos = [s[0].pos for s in self.r]
+            vec = [s[0].move for s in self.r]
+
+            c = (np.average(pos,axis = 0) - self.pos) - self.move #fc - vel --> coheison
+            s = np.average([self.pos - x for x in pos], axis = 0) #seperation
+            a = np.average(vec, axis = 0) - self.move #alignment
+
+            f_total = (self.config.alignment_weight * a +
+                       self.config.separation_weight * s +
+                       self.config.cohesion_weight * c +
+                       self.config.random_weight * np.random.uniform(low = -1, high = 1, size = 2)) / self.config.mass
+
+        else: f_total = (self.config.random_weight * np.random.uniform(low = -1, high = 1, size = 2)) / self.config.mass
         self.move += f_total
 
-    def update_location(self):
         self.pos += self.move * self.config.delta_time
+
 
     def change_position(self):
         self.there_is_no_escape()
         if self.age > self.max_age: self.kill()
         if self.is_alive():
+<<<<<<< Updated upstream
 
             if self.age > self.breed_age and np.random.uniform() < self.p_reproduction: self.reproduce()
             self.random_move()
             self.update_location()
             self.age += 1
+=======
+            self.r = list(self.in_proximity_accuracy())
+
+            prob = self.p_reproduction/(len(self.r)) if len(self.r) > 0 else self.p_reproduction
+            if np.random.uniform() < prob: self.reproduce()
+            self.random_move()
+
+>>>>>>> Stashed changes
 
 
 
@@ -117,9 +185,14 @@ df = (
             duration=50000,
             movement_speed=1,
             image_rotation=True,
+<<<<<<< Updated upstream
             radius=5,
             seed=GLOBAL_SEED,
             print_fps = True
+=======
+            radius=17,
+            seed=GLOBAL_SEED
+>>>>>>> Stashed changes
         )
     )
         .batch_spawn_agents(500, Rabbit, images=["images/white.png","images/red.png","images/green.png"])
@@ -127,7 +200,10 @@ df = (
         .run()
 )
 
-
 dfs = df.snapshots
+<<<<<<< Updated upstream
 dfs.write_csv("X.csv")
+=======
+dfs.write_parquet(f"X_{GLOBAL_SEED}.pqt")
+>>>>>>> Stashed changes
 
